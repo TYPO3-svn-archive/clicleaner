3a9a3331585f824abe9064b33b25d2e41d2c7743
arndmesser <arndmesser@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-06 17:13:57 +0000

    initial forge commit
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/clicleaner/trunk@41840 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,49 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "clicleaner".
+#
+# Auto generated 06-01-2011 17:01
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Cli Cleaner',
+	'description' => 'Automation tool for clearing up TYPO3 cache and indexed_search index. For use with lowlevel cleaner.',
+	'category' => 'be',
+	'shy' => 0,
+	'version' => '1.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Arnd Messer [die medienagenten]',
+	'author_email' => 'typo3@medienagenten.de',
+	'author_company' => 'die medienagenten',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.0.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"0b8a";s:10:"README.txt";s:4:"97a8";s:29:"class.tx_clicleaner_cache.php";s:4:"590f";s:29:"class.tx_clicleaner_index.php";s:4:"bd46";s:12:"ext_icon.gif";s:4:"cc76";s:17:"ext_localconf.php";s:4:"e647";s:13:"locallang.xml";s:4:"7951";}',
+	'suggests' => array(
+	),
+);
+
+?>
